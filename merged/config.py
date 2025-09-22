c04b58ef1ec5b24a21a96e3ff7019ceae9d6deeb
Akshita Gupta <akshita24680@gmail.com>
2025-09-22 01:01:09 -0400

    New folder for merged pipeline

---
+++ merged/config.py
@@ -0,0 +1,34 @@
+DOMAIN = 'raceXGender'
+PROMPT_FILE = 'prompts_bbq_raceXGender.json'
+DATA_FILE = 'data_bbq.json'
+NUM_EX = 1
+EX_IDXS = range(0, NUM_EX)
+NUM_CF = 1
+BALANCED = False
+MIXED = False
+WITH_CONTEXT = True
+EXPLANATION = True
+'''implement stratified sampling in simqa'''
+# STRATIFIED = False
+# STRAT_SAMPLES_PER_OPTION = 1
+# TOTAL_STRAT_SAMPLES = 3
+
+taskqa_models = ["gpt-4o-mini"]
+expl_types = ["cot"]
+simqg_models = ["gpt-4o-mini"]
+simqa_models = ["gpt-4o-mini"]
+
+def print_configs():
+    print("\n\033[1mCurrent Configurations:\033[0m")
+    print(f"DOMAIN: {DOMAIN}")
+    print(f"NUM_EX: {NUM_EX}")
+    print(f"EX_IDXS: {list(EX_IDXS)}")
+    print(f"NUM_CF: {NUM_CF}")
+    print(f"WITH_CONTEXT: {WITH_CONTEXT}")
+    print(f"EXPLANATION: {EXPLANATION}")
+    print(f"MIXED: {MIXED}")
+    print(f"BALANCED: {BALANCED}")
+    print(f"taskqa_models: {taskqa_models}")
+    print(f"expl_types: {expl_types}")
+    print(f"simqg_models: {simqg_models}")
+    print(f"simqa_models: {simqa_models}")
---
+++ merged/prompts/load_prompt.py
@@ -0,0 +1,31 @@
+import json
+import os
+import config
+
+prompt_file = config.PROMPT_FILE
+
+def _embed_prompt(
+    instruction, template_with_label, template_no_label, dem_examples, test_example
+):
+    prompt = str(instruction)
+    for dem in dem_examples:
+        prompt += template_with_label.format(**dem)
+
+    # prompt += template_no_label.format(**test_example).strip()
+    prompt += template_no_label.format(**test_example)
+    return prompt
+
+
+def get_prompts_by_task(task, test_examples, k_shot=None):
+    prompt = json.load(open(os.path.join(os.path.dirname(__file__), prompt_file)))[task]
+    # if len(test_examples) > 0:
+    #     print(_embed_prompt(prompt['instruction'], prompt['template_with_label'],
+    #                           prompt['template_no_label'], prompt['dem_examples'], test_examples[0]))
+    #     exit()
+    dem_examples = prompt['dem_examples']
+    if k_shot is not None:
+        dem_examples = dem_examples[:k_shot]
+    return [_embed_prompt(prompt['instruction'], prompt['template_with_label'],
+                          prompt['template_no_label'], dem_examples, test_example)
+            for test_example in test_examples]
+
